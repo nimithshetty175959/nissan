38c5802002063a7f5ece8f26f3a1a7c702fa5405
sivachandran <siva.chandran@ust.com>
2024-11-21 19:01:41 +0530

    footer-section

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -14,7 +14,41 @@ export default async function decorate(block) {
   // decorate footer DOM
   block.textContent = '';
   const footer = document.createElement('div');
+  footer.classList.add('footer-section-one');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
   block.append(footer);
+
+  const parentDiv = document.querySelector('.footer-section-one');
+  let childDivs;
+  if (parentDiv) {
+    childDivs = parentDiv.querySelectorAll('.section');
+  }
+  if (childDivs.length >= 2) {
+    childDivs[childDivs.length - 3].classList.add('social-media-links');
+    childDivs[childDivs.length - 2].classList.add('section-two');
+    childDivs[childDivs.length - 1].classList.add('section-two', 'footer-legal');
+  }
+
+  const secondfooter = parentDiv.querySelectorAll('.section-two');
+  const newContainer = document.createElement('div');
+  newContainer.classList.add('footer-section-two');
+  secondfooter.forEach(element => {
+    newContainer.append(element);
+  });
+   document.body.appendChild(newContainer);
+   const targetDiv = document.querySelector('.footer');
+   targetDiv.appendChild(newContainer);
+ 
+ 
+   // Social media links
+   const links = document.querySelectorAll('.social-media-links p');
+   const linksWrapper = document.createElement('div');
+   links.forEach(element => {
+     linksWrapper.append(element);
+   });
+   linksWrapper.classList.add("social-media-wrapper");
+   const targetElement = document.querySelector('#hero-on-social-media');
+   targetElement.insertAdjacentElement('afterend', linksWrapper); 
+ 
 }
