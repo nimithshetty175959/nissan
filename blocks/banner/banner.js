a6904e9ab0bd778d6dbd49326965ecb956b354ea
Kiran Bogadi <kiran.b@ust.com>
2024-11-22 16:10:19 +0530

    Banner js fix

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,17 +1,15 @@
 import { createBanerItem, getCarouselNav } from './bannerHelper.js';
 
 export default function decorate(block) {
-  if (window.location.hostname.indexOf('author') === -1) {
-    const copyBlock = block.cloneNode(true);
-    const copyBlockNav = block.cloneNode(true);
-    const elements = copyBlock.children;
-    block.innerHTML = '';
-    for (let index = 0; index < elements.length; index += 1) {
-      const element = elements[index];
-      const bannerItem = createBanerItem(element, index);
-      block.append(bannerItem);
-    }
-    const carNav = getCarouselNav(copyBlockNav);
-    block.append(carNav);
+  const copyBlock = block.cloneNode(true);
+  const copyBlockNav = block.cloneNode(true);
+  const elements = copyBlock.children;
+  block.innerHTML = '';
+  for (let index = 0; index < elements.length; index += 1) {
+    const element = elements[index];
+    const bannerItem = createBanerItem(element, index);
+    block.append(bannerItem);
   }
+  const carNav = getCarouselNav(copyBlockNav);
+  block.append(carNav);
 }
